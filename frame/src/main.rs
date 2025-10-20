6d5254b0e3111f6f0ce5a14e895589232d0df813
harry-drummond <harry@drummail.co.uk>
2025-10-20 22:29:39 +0100

    Updated inky runnable to pick a random image from the api

--- frame/src/main.rs
+++ frame/src/main.rs
@@ -1,9 +1,16 @@
 mod inky;
 
+use std::env;
+use rand::Rng;
+use serde::Deserialize;
+use reqwest;
+
 use crate::inky::Inky;
 
 #[tokio::main]
 async fn main(){
+
+    let image = fetch_image_from_api().await;
     let mut inky = Inky::new();
     inky.setup().await;
     
@@ -11,4 +18,29 @@ async fn main(){
     inky.set_image(path, true);
 
     inky.show().await;
+}
+
+// Pull images from API.
+async fn fetch_image_from_api() -> String {
+    // TODO: Fall back - if call fails, show error image.
+    let api_base_url = env::var("API_BASE_URL").unwrap();
+    let api_endpoint = api_base_url + "/images";
+
+    let response = reqwest::get(api_endpoint).await
+        .expect("Failed to fetch images")
+        .json::<Vec<Image>>()
+        .await
+        .expect("Failed to parse JSON response");
+
+    let mut rng = rand::rng();
+
+    let random_image_index = rng.random_range(0..response.len());
+
+    response[random_image_index].file_path.clone()
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Image {
+    #[serde(rename = "filePath")]
+    pub file_path: String
 }
